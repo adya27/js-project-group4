a58bbed430ab9f5110b81ab2c6f7616c66f78076
adya27 <mdipk4t@gmail.com>
2020-11-17 16:19:49 +0200

    add fetch and jenres

---
+++ src/javascripts/fetch.js
@@ -0,0 +1,29 @@
+const apiKey = `00247c3a79a23ea8d225678fa2dae566`;
+let query = `js`; // takes from form
+let page = 1; // pagination while search movie
+let movieId = 701189; // data-attribute of the backdrop img
+
+const fetchIt = URL => {
+  fetch(URL).then(res => res.json());
+  // .then(src => console.log(src.results));
+};
+
+const makesTrendingMkp = () => {
+  const URLTrending = `https://api.themoviedb.org/3/trending/all/day?api_key=${apiKey}`; //constant
+
+  fetchIt(URLTrending).then(src => movieCardMkp(src.results)); //сделать хендлбар функцию movieCardMkp для одной карточки
+};
+
+const makesSearchMkp = (query, page) => {
+  const URLSearch = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=${page}&include_adult=false`;
+
+  fetchIt(URLSearch).then(src => movieCardMkp(src.results)); //сделать хендлбар функцию movieCardMkp для одной карточки
+};
+
+const makesModalMkp = movieId => {
+  const URLMovie = `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}&language=en-US`;
+
+  fetchIt(URLSearch).then(src => movieModalCardMkp(src.results)); //сделать хендлбар функцию movieModalCardMkp для МОДАЛКИ
+};
+
+// fetchTrending(URLTrending);
