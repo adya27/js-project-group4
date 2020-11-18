a43081cfa50d3add137460e06c2300181df555e6
Vitaliy Zvieriev <zverew91@gmail.com>
2020-11-18 20:18:09 +0200

    Добавляет разметку header-myLibrary

---
+++ src/myLibrary.js
@@ -0,0 +1,7 @@
+import './sass/myLibrary.scss';
+
+import './javascripts/fetch';
+import './javascripts/givesGenresNames';
+import './javascripts/loader';
+import './javascripts/search-movies';
+import './javascripts/addTrendsMkp';
