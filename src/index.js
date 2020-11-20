5b55cb929116c71020240b867b05feb612393619
Irina-Koksharova <biodgerelo@ukr.net>
2020-11-20 19:27:31 +0200

    Добавляет пагинацию при поиске

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './javascripts/fetch';
 import './javascripts/givesGenresNames';
 import './javascripts/loader';
 
-// import './javascripts/search-movies';
+import './javascripts/search-movies';
 import './javascripts/addTrendsMkp';
-import './javascripts/search-movies-pagination';
+// import './javascripts/search-movies-pagination';
 
 import './javascripts/api-service';
 
