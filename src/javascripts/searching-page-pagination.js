652b5f3a405447a62f1d3de9a3ea3f1742b801db
Irina-Koksharova <biodgerelo@ukr.net>
2020-11-22 06:09:27 +0200

    Добавляет разметку для пагинации при поиске

--- src/javascripts/searching-page-pagination.js
+++ src/javascripts/searching-page-pagination.js
@@ -1,25 +1,50 @@
-import paginationCard from '../templates/pagination.hbs';
+import paginationCardSmall from '../templates/pagination-search-small.hbs';
+import paginationCardInternal from '../templates/pagination-search-internal.hbs';
+import paginationCardExternal from '../templates/pagination-search-external.hbs';
 import { imageApiService, refs, renderMarkup } from './search-movies';
 
 function makesPagination() {
     for (let i = 0; i < imageApiService.totalPages.length; i += 1) {
         imageApiService.totalPages[i] = i + 1;
-        }
-    refs.pageButton.insertAdjacentHTML('beforeend', paginationCard(imageApiService.totalPages));
+    }
+    if (imageApiService.totalPages.length <= 5) {
+        refs.pageButton.insertAdjacentHTML('beforeend', paginationCardSmall(imageApiService.totalPages));
+    } else if (document.documentElement.clientWidth <= 320) {
+        refs.pageButton.insertAdjacentHTML('beforeend', paginationCardSmall(imageApiService.totalPages));
+    } else {
+        refs.stableContaiverPrev.insertAdjacentHTML('afterbegin', paginationCardInternal());
+        refs.pageButton.insertAdjacentHTML('afterbegin', paginationCardSmall(imageApiService.totalPages));
+        refs.stableContaiverNext.insertAdjacentHTML('afterbegin', paginationCardExternal());
+        
+        const lastPage = document.querySelector('.last-page');
+        lastPage.textContent = imageApiService.totalPages.length;
+    }
     refs.pageButton.addEventListener('click', changePage);
-
     refs.buttonNext.addEventListener('click', nextPage);
     refs.buttonPrev.addEventListener('click', previousPage);
+    const firstPage = document.getElementById(`${imageApiService.totalPages[0]}`);
+    firstPage.classList.add('current-page');
 }
 
+// let idClick = 1;
+
 function nextPage() {
-    refs.pageButton.style.transform = moveRight();   
+    refs.pageButton.style.transform = moveRight(); 
+    // idClick += 1;
+    // if (idClick === clickNumber) {
+    //     refs.buttonNext.removeEventListener('click', nextPage);
+    //     idClick = 1;
+    // }
+    // return idClick;   
 }
 
 let width = 0;
+
 function moveRight() {
-    for (let i = 0; i < 25; i += 1) {
-        width += 400;
+    const block = 10;
+    const blockNumber = imageApiService.totalPages.length % block;
+    for (let i = 0; i < blockNumber + 1; i += 1) {
+        width += 200;
         const move = `translateX(-${width}px)`;
         return move;
     }
@@ -27,12 +52,17 @@ function moveRight() {
 }
 
 function previousPage() {
-    refs.pageButton.style.transform = moveLeft();   
+    refs.pageButton.style.transform = moveLeft(); 
+    // idClick += 1;
+    // if (idClick === clickNumber) {
+    //     refs.buttonNext.removeEventListener('click', nextPage);
+    // }
+    // return idClick;    
 }
 
 function moveLeft() {
     for (let i = 0; i < 5; i += 1) {
-        width -= 400;
+        width -= 200;
         const move = `translateX(-${width}px)`;
         return move;
     }
@@ -44,8 +74,14 @@ function resetMove() {
 }
 
 function changePage(e) {
+    for (let i = 0; i < refs.pageButton.children.length; i += 1) {
+        if (refs.pageButton.children[i].classList.contains('current-page')) {
+            refs.pageButton.children[i].classList.remove('current-page');
+        }
+    }
+    e.target.closest('li').classList.add('current-page');
     imageApiService.setPage(e.target.textContent);
-    imageApiService.fetchImages().then(renderMarkup);   
+    imageApiService.fetchImages().then(renderMarkup);  
 }
 
 
@@ -59,6 +95,8 @@ function scrollMovies() {
 function clearPagination() {
     imageApiService.resetPage();
     refs.pageButton.innerHTML = '';
+    refs.stableContaiverPrev.innerHTML = '';
+    refs.stableContaiverNext.innerHTML = '';
 }
 
-export { makesPagination, nextPage, moveRight, previousPage, moveLeft, changePage, scrollMovies, clearPagination, resetMove };
+export { makesPagination, scrollMovies, clearPagination, resetMove };
